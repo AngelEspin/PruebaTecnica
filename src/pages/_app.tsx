6dcf05622e27c84e42338cc5558c6c229d6a6a62
AngelEspin <angelumb@espol.edu.ec>
2024-11-19 11:28:03 -0500

    Changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // src/pages/_app.tsx
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store'; // Importa tu store
+import { store } from '../redux/store'; // Importa store
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
